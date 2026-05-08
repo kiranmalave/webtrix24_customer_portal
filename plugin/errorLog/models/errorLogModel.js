0d5b735f13d4e9b2766e260fe6b108cb6c22efa5
Kiran Malave <kiran.malave@gmail.com>
2026-05-08 15:47:33 +0530

    project clean up.

--- plugin/errorLog/models/errorLogModel.js
+++
@@ -1,9 +0,0 @@
-define([
-    'underscore',
-    'backbone',
-], function(_, Backbone) {
-    var errorLogModels = Backbone.Model.extend({
-        idAttribute: "errorID",
-    });
-    return errorLogModels;
-});
